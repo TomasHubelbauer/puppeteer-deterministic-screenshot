5b1c1bdb770ff61b63d7b07cc9514dfdf58ff7c5
Tomas Hubelbauer <tomas@hubelbauer.net>
2020-04-29 20:54:53 +0200

    Document the types of comparisons and Windows results

--- README.md
+++ README.md
@@ -3,3 +3,34 @@
 Are Puppeteer screenshots deterministic given the same input HTML? Let's find
 out by setting up Github Actions to run the same Puppeteer screenshot generating
 script and comparing the binary files.
+
+## Deterministic Across OSs
+
+It is impossible for the screenshots to be deterministic across OSs due to the
+different text rendering technologies employed by the different OSs.
+
+This work is not exploring this, but there might be an angle in forcing the same
+font across OSs and forcing software rendering in Chrome, which might fall back
+onto the same text renderer.
+
+## Deterministic Across Runs on the Same OS
+
+This is what we're after: if the same script is ran twice on the same machine
+and the same HTML is used, will the generated screenshot file be the same?
+
+Is this true for all OSs?
+
+| OS      | Result                               |
+|---------|--------------------------------------|
+| Windows | Screenshots are the same across runs |
+| Linux   | ?                                    |
+| macOS   | ?                                    |
+
+## To-Do
+
+### Consider looking into the software rendering angle described above
+
+### Fix the GitHub Actions Git push failing due to concurency issues (probably)
+
+Maybe store in GitHub Actions artifacts as an alternative, but I'd like to fix
+this too, as it should come in handy.
--- index.js
+++ index.js
@@ -1,15 +1,43 @@
 const puppeteer = require('puppeteer');
 const path = require('path');
+const fs = require('fs-extra');
 
 // Crash on errors
 process.on('unhandledRejection', error => { throw error; });
 process.on('uncaughtException', error => { throw error; });
 
 void async function () {
+  console.log('Checking PNG');
+  await check('png');
+  console.log('Checking JPEG');
+  await check('jpeg');
+}()
+
+async function run(/** @type {String} */ path, /** @type {'png' | 'jpeg'} */ type) {
   const browser = await puppeteer.launch();
   const [page] = await browser.pages();
   await page.goto('https://news.ycombinator.com');
-  await page.screenshot({ path: path.join(__dirname, 'screenshot-' + process.platform + '.png'), type: 'png' });
-  await page.screenshot({ path: path.join(__dirname, 'screenshot-' + process.platform + '.jpg'), type: 'jpeg' });
+  await page.screenshot({ path, type });
   await browser.close();
-}()
+}
+
+async function check(/** @type {'png' | 'jpeg'} */ type) {
+  const screenshotPath = path.join(__dirname, 'screenshot-' + process.platform + '.' + type);
+
+  await run(screenshotPath, type);
+  const initialBuffer = await fs.readFile(screenshotPath);
+
+  await run(screenshotPath, type);
+  const comparisonBuffer = await fs.readFile(screenshotPath);
+
+  const length = Math.max(initialBuffer.byteLength, comparisonBuffer.byteLength);
+  const differences = 0;
+  for (let index = 0; index < length; index++) {
+    if (initialBuffer[index] !== comparisonBuffer[index]) {
+      console.log(index, initialBuffer[index], comparisonBuffer[index]);
+      differences++;
+    }
+  }
+
+  console.log(differences, 'differences');
+}
